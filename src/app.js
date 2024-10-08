3ac67fdc6ef278a350007218139630fc69dedc6d
b1gd3vd0g <devin@bigdevdog.com>
2024-10-07 17:20:28 -0700

    Add a BrowserRouter to the app component which links to the HomePage (at the root path) and the 404 page (any other path)

--- src/app.js
+++ src/app.js
@@ -1,9 +1,25 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import FourOhFour from './404';
+import HomePage from './home_page';
+
 const App = () => {
-    return (
-        <div className='App'>
-            <h1>App</h1>
-        </div>
-    );
+  return (
+    <div className='App'>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage />}
+          />
+          <Route
+            path='*'
+            element={<FourOhFour />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
